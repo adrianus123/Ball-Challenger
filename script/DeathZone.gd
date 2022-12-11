017770400f29f49949393fd3e685f0e8d021f3c4
adrianus123 <adrianussimarmata9@gmail.com>
2022-12-11 12:53:30 +0700

    initial commit

---
+++ script/DeathZone.gd
@@ -0,0 +1,5 @@
+extends Area
+
+func _on_DeathZone_body_entered(body):
+	if body.name == "Ball":
+		get_tree().reload_current_scene()
---
+++ script/MainMenu.gd
@@ -0,0 +1,31 @@
+extends Spatial
+
+onready var start_button = $CanvasLayer/Fader/Control/VBoxContainer/CenterContainer/VBoxContainer/StartButton
+onready var help_button = $CanvasLayer/Fader/Control/VBoxContainer/CenterContainer/VBoxContainer/HelpButton
+onready var about_button = $CanvasLayer/Fader/Control/VBoxContainer/CenterContainer/VBoxContainer/AboutButton
+onready var quit_button = $CanvasLayer/Fader/Control/VBoxContainer/CenterContainer/VBoxContainer/QuitButton
+onready var fader = $CanvasLayer/Fader
+
+export (PackedScene) var game_scene = null
+
+func _ready():
+	start_button.connect("pressed", self, "on_start_pressed")
+	help_button.connect("pressed", self, "on_help_pressed")
+	about_button.connect("pressed", self, "on_about_pressed")
+	quit_button.connect("pressed", self, "on_quit_pressed")
+	fader.connect("fade_finished", self, "on_fade_finished")
+
+func on_start_pressed():
+	fader.fade_in()
+
+func on_help_pressed():
+	pass
+
+func on_about_pressed():
+	pass
+
+func on_quit_pressed():
+	get_tree().quit()
+
+func on_fade_finished():
+	get_tree().change_scene_to(game_scene)
